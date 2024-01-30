7a387e5621184120e618e2577eba0931aaf5c09a
LinhDoVu <vulinh102ii@gmail.com>
2024-01-30 17:32:14 +0700

    add service

---
+++ src/main/java/com/example/payrollez/service/Payroll/IPayrollService.java
@@ -0,0 +1,21 @@
+package com.example.payrollez.service.Payroll;
+
+import com.example.payrollez.model.Payroll;
+import com.example.payrollez.service.IService;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IPayrollService extends IService<Payroll,Long> {
+    @Override
+    Optional<Payroll> findById(Long aLong);
+
+    @Override
+    List<Payroll> findAll();
+
+    @Override
+    Payroll save(Payroll payroll);
+
+    @Override
+    void delete(Long aLong);
+}
