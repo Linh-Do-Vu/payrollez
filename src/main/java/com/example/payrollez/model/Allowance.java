3206fad009056882702b041d828ac223c45b6991
LinhDoVu <vulinh102ii@gmail.com>
2024-07-28 09:34:25 +0700

    model linhtestn3234

--- src/main/java/com/example/payrollez/model/Allowance.java
+++ src/main/java/com/example/payrollez/model/Allowance.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @Table(name = "Allowance")
 public class Allowance {
 //model linhtest
+//    test commit2 2
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "allowanceid")
